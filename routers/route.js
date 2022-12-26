1e2dbe84f7a4a50ea1962d2b91f427af503cd323
psh2661 <psh2661@gmail.com>
2022-12-26 11:28:08 +0900

    라우터 추가

---
+++ routers/route.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('./../db.js');
+
+const html = `<!doctype html>
+  <html>
+  <head>
+  <title>node HTML</title>
+  </head>
+  <body>
+  <h1>hello</h1>
+  <form action='/name' method ='POST'>
+  <input type='text' name='name' placeholder='이름'>
+  <button> 확인 </button>
+  </form>
+  </body>
+  </html>`;
+  //메인페이지 연결
+  router.get('/',(req, res) => {
+    res.render('main'); 
+  })
+  
+  //공지사항 연결
+  //리스트 페이지
+  router.get('/notice_list',(req, res) => {
+    res.render('notice_list'); 
+  })
+  //작성 페이지
+  router.get('/notice_write',(req, res) => {
+    res.render('notice_write'); 
+  })
+  //상세 페이지
+  router.get('/notice_view',(req, res) => {
+    res.render('notice_view'); 
+  })
+  //수정 페이지
+  router.post('/writeMemo',(req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let title = param['title'];//param중에 머머..
+    let name = param['name'];
+    let date = param['date'];
+    let content = param['content'];
+    let pw = param['pw'];
+    console.log(title);
+    console.log(name);
+    console.log(date);
+    console.log(content);
+    console.log(pw);
+    res.render('notice_view',{'data':param});//내가 원하는 페이지로 보내줘
+  });
+  
+  //로그인 연결
+  router.get('/login',(req, res) => {
+    res.render('login'); 
+  });
+  router.post('/login',(req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let id = param['id'];
+    let password = param['password'];
+    console.log(id);
+    console.log(password);
+    
+  });
+  router.get('/login',(req, res) => {
+    db.getMemo((rows)=>{
+      res.render('login',{rows:rows});
+    })
+  
+  });
+  router.post('/login',(req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let id = param['id'];
+    let password = param['password'];
+    console.log(id);
+    console.log(password);
+    
+  });
+
+  //회원가입 연결 - 이용약관
+  router.get('/join01',(req, res) => {
+    res.render('join01'); 
+  });
+  router.post('/join01',(req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let agree01 = param['agree01'];
+    let agree02 = param['agree02'];
+    let agree03 = param['agree03'];
+    let agree_all = param['agree_all'];
+    console.log(agree01);
+    console.log(agree02);
+    console.log(agree03);
+    console.log(agree_all);
+  });
+  //회원가입 연결 - 정보입력
+  router.get('/join02',(req, res) => {
+    res.render('join02'); 
+  });
+  router.post('/join02',(req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let user_ID = param['user_ID'];
+    let user_PW1 = param['user_PW1'];
+    let user_PW2 = param['user_PW2'];
+    let user_name = param['user_name'];
+    let user_email = param['user_email'];
+    let user_phone = param['user_phone'];
+    console.log(user_ID);
+    console.log(user_PW1);
+    console.log(user_PW2);
+    console.log(user_name);
+    console.log(user_email);
+    console.log(user_phone);
+  });
+  
+  module.exports = router;
